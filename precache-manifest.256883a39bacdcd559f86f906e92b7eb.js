2458491c1acbf9cb0decab7db8ba50e9389c6775
Manikandan Kaliyappan <contact.manisoft@gmail.com>
2020-04-11 18:03:18 +0530

    Updates

---
+++ precache-manifest.256883a39bacdcd559f86f906e92b7eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc531d84fc5ac37c0627e478bc993c0c",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "d45a5430cb9060b03670",
+    "url": "/youtube-clone/static/css/main.1f4e51da.chunk.css"
+  },
+  {
+    "revision": "71ce3470c5c992d99445",
+    "url": "/youtube-clone/static/js/2.28cd95e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-clone/static/js/2.28cd95e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45a5430cb9060b03670",
+    "url": "/youtube-clone/static/js/main.6c2d9f18.chunk.js"
+  },
+  {
+    "revision": "dc97c32b347202ef8e78",
+    "url": "/youtube-clone/static/js/runtime-main.aba7e4c6.js"
+  }
+]);
